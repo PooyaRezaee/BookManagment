08a41a3704939f5e2cf4a7d264d6c6fb031b3931
PooyaRezaee <pooya.rezaee.official@gmail.com>
2023-02-18 19:10:41 +0330

    Complate tests

--- apps/pages/tests.py
+++ apps/pages/tests.py
@@ -1,15 +1,18 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse,resolve
+from .views import HomeView
 
 class HomepageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        url = reverse("page:index")
+        self.response = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse("page:index"))
-        self.assertEqual(response.status_code, 200)
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "index.html")
+        self.assertTemplateUsed(self.response, "index.html")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomeView.as_view().__name__)
