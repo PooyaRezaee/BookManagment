c05e223711213ccfd28ff4ff7d82b9e27b87239c
PooyaRezaee <pooya.rezaee.official@gmail.com>
2023-02-21 22:22:15 +0330

    Added DetailView Book and Fix Routing

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 
+app_name = 'book'
+
 urlpatterns = [
-    path("", BookListView.as_view(), name="book-list"),
+    path("", BookListView.as_view(), name="list"),
+    path('<int:pk>/', BookDetailView.as_view(), name='detail'),
 ]
--- apps/books/views.py
+++ apps/books/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Book
 
 __all__ = [
     'BookListView',
+    'BookDetailView'
 ]
 
 class BookListView(ListView):
     template_name = 'books/list.html'
     model = Book
     context_object_name = 'books'
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = "books/detail.html"
