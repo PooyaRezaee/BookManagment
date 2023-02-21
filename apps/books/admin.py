16cd2187efb992377edc35fcfcd3cf1eebe01492
PooyaRezaee <pooya.rezaee.official@gmail.com>
2023-02-21 21:56:21 +0330

    Added Model Book and Register On admin Panel

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author','price','added_at','discount')
+    list_filter = ('title','author','added_at')
--- apps/books/models.py
+++ apps/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    added_at = models.DateTimeField(auto_now_add=True)
+    discount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
